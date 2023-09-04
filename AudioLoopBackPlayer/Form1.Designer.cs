c2479c480a5c1189fe160f20679e960abf8ec649
Hassan Ansari <33068452+hassanansari92@users.noreply.github.com>
2023-09-04 08:47:13 +0500

    Add project files.

---
+++ AudioLoopBackPlayer/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace AudioLoopBackPlayer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtName = new TextBox();
+            btnBrowse = new Button();
+            SuspendLayout();
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(12, 31);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(483, 27);
+            txtName.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            btnBrowse.Location = new Point(501, 31);
+            btnBrowse.Name = "btnBrowse";
+            btnBrowse.Size = new Size(94, 29);
+            btnBrowse.TabIndex = 1;
+            btnBrowse.Text = "Browse";
+            btnBrowse.UseVisualStyleBackColor = true;
+            btnBrowse.Click += btnBrowse_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(621, 427);
+            Controls.Add(btnBrowse);
+            Controls.Add(txtName);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox txtName;
+        private Button btnBrowse;
+    }
+}
---
+++ AudioLoopBackPlayer/Form1.cs
@@ -0,0 +1,21 @@
+using System.Windows.Forms;
+
+namespace AudioLoopBackPlayer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                txtName.Text = openFileDialog.FileName;
+            }
+        }
+    }
+}
